a9da92709883a47be28f4c720678e77318e21d60
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-03 20:11:57 +0700

    Update json_test.go

--- json_test.go
+++ json_test.go
@@ -1,9 +1,27 @@
 package gojson
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 )
 
-func TestEndcode(t *testing.T) {
+func errHandler(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
 
+func logJSON(data interface{}) {
+	bytes, err := json.Marshal(data)
+	errHandler(err)
+
+	fmt.Println(string(bytes))
+}
+
+func TestEndcode(t *testing.T) {
+	logJSON("Arif")
+	logJSON(1)
+	logJSON(true)
+	logJSON([]string{"Arif", "Rachman", "Hakim"})
 }
