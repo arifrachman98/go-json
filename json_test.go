66674150882d33070f61d1d9e0645e495231360b
Arif Rachman Hakim <arifrachman149@gmail.com>
2023-01-03 21:01:55 +0700

    Update json_test.go

--- json_test.go
+++ json_test.go
@@ -6,12 +6,20 @@ import (
 	"testing"
 )
 
+type Address struct {
+	Street     string
+	Country    string
+	PostalCode string
+}
+
 type Customer struct {
-	FName   string
-	MName   string
-	LName   string
-	Age     int
-	Married bool
+	FName     string
+	MName     string
+	LName     string
+	Age       int
+	Married   bool
+	Hobbies   []string
+	Addresses []Address
 }
 
 func errHandler(err error) {
@@ -27,13 +35,6 @@ func logJSON(data interface{}) {
 	fmt.Println(string(bytes))
 }
 
-func TestEndcode(t *testing.T) {
-	logJSON("Arif")
-	logJSON(1)
-	logJSON(true)
-	logJSON([]string{"Arif", "Rachman", "Hakim"})
-}
-
 func TestJSONObj(t *testing.T) {
 	cust := Customer{
 		FName:   "Arif",
@@ -47,6 +48,13 @@ func TestJSONObj(t *testing.T) {
 	errHandler(err)
 	fmt.Println(string(bytes))
 
+}
+
+func TestEndcode(t *testing.T) {
+	logJSON("Arif")
+	logJSON(1)
+	logJSON(true)
+	logJSON([]string{"Arif", "Rachman", "Hakim"})
 }
 
 func TestDecode(t *testing.T) {
@@ -70,4 +78,133 @@ func TestDecode(t *testing.T) {
 	fmt.Println(cust.LName)
 	fmt.Println(cust.Age)
 	fmt.Println(cust.Married)
+}
+
+func TestJSONArrayEncode(t *testing.T) {
+	cust := Customer{
+		FName:   "Arif",
+		MName:   "Torenoo",
+		LName:   "Hakim",
+		Age:     25,
+		Married: true,
+		Hobbies: []string{"Gimang", "Geming", "Gemung"},
+	}
+
+	bytes, err := json.Marshal(cust)
+	errHandler(err)
+	fmt.Println(string(bytes))
+}
+
+func TestJSONArrayDecode(t *testing.T) {
+	JSONstring := `{
+		"FName" : "Arif",
+		"MName" : "Rachman",
+		"LName" : "Hakim",
+		"Age" : 24,
+		"Married" : false,
+		"Hobbies" : ["Gem","Makan","Code"]
+	}`
+	JSONbytes := []byte(JSONstring)
+
+	cust := &Customer{}
+
+	err := json.Unmarshal(JSONbytes, cust)
+	errHandler(err)
+
+	fmt.Println(cust)
+	fmt.Println(cust.FName)
+	fmt.Println(cust.MName)
+	fmt.Println(cust.LName)
+	fmt.Println(cust.Age)
+	fmt.Println(cust.Married)
+	fmt.Println(cust.Hobbies)
+}
+
+func TestJSONArrayComplexEncode(t *testing.T) {
+	cust := Customer{
+		FName:   "Arif",
+		MName:   "Torenoo",
+		LName:   "Hakim",
+		Age:     25,
+		Married: true,
+		Hobbies: []string{"Gimang", "Geming", "Gemung"},
+		Addresses: []Address{
+			{
+				Street:     "Jl. Bengawan",
+				Country:    "Indonesia",
+				PostalCode: "171745",
+			},
+			{
+				Street:     "Lomere",
+				Country:    "Koba",
+				PostalCode: "100203",
+			},
+			{
+				Street:     "Poerto",
+				Country:    "Ricco",
+				PostalCode: "14045",
+			},
+		},
+	}
+
+	bytes, err := json.Marshal(cust)
+	errHandler(err)
+	fmt.Println(string(bytes))
+}
+
+func TestJSONArrayComplexDecode(t *testing.T) {
+	JSONstring := `{
+		"FName" : "Arif",
+		"MName" : "Rachman",
+		"LName" : "Hakim",
+		"Age" : 24,
+		"Married" : false,
+		"Hobbies" : ["Gem","Makan","Code"],
+		"Addresses" : [{"Street" : "Jalanan","Country" : "Lomestry","PostalCode" : "44697"},{"Street" : "Jalanan yang jauh","Country" : "Panama","PostalCode" : "569778"},{"Street" : "Jalanan yang rusak","Country" : "Armedia","PostalCode" : "5489486"}]
+	}`
+	JSONbytes := []byte(JSONstring)
+
+	cust := &Customer{}
+
+	err := json.Unmarshal(JSONbytes, cust)
+	errHandler(err)
+
+	fmt.Println(cust)
+	fmt.Println(cust.FName)
+	fmt.Println(cust.MName)
+	fmt.Println(cust.LName)
+	fmt.Println(cust.Age)
+	fmt.Println(cust.Married)
+	fmt.Println(cust.Hobbies)
+	fmt.Println(cust.Addresses)
+}
+
+func TestJSONOnlyArrayComplexEncode(t *testing.T) {
+	addr := []Address{
+		{
+			Street:     "Lomere",
+			Country:    "Koba",
+			PostalCode: "100203",
+		},
+		{
+			Street:     "Poerto",
+			Country:    "Ricco",
+			PostalCode: "14045",
+		},
+	}
+
+	bytes, err := json.Marshal(addr)
+	errHandler(err)
+	fmt.Println(string(bytes))
+}
+
+func TestJSONOnlyArrayComplexDecode(t *testing.T) {
+	JSONstring := `[{"Street" : "Jalanan","Country" : "Lomestry","PostalCode" : "44697"},{"Street" : "Jalanan yang jauh","Country" : "Panama","PostalCode" : "569778"},{"Street" : "Jalanan yang rusak","Country" : "Armedia","PostalCode" : "5489486"}]`
+	JSONbytes := []byte(JSONstring)
+
+	addr := &[]Address{}
+	err := json.Unmarshal(JSONbytes, addr)
+	errHandler(err)
+
+	fmt.Println(addr)
 }
